3210a102839813f2fe25b45b67fb9ae3100a01d8
Daniela Quinteros <danielaquinteros.a@gmail.com>
2023-10-31 22:07:58 -0300

    ultima act

--- public/signUp/signUp.js
+++ public/signUp/signUp.js
@@ -2,33 +2,33 @@ const buttonRegister = document.querySelector(".boton");
 
 // Generamos una funcion para guardar los valores que ingresa el usuario
 function getInputValues() {
-    // Obtener los input del form
-    const nombreInput = document.querySelector("#nombre");
-    const apellidoInput = document.querySelector("#apellido");
-    const emailInput = document.querySelector("#email");
-    const passwordInput = document.querySelector("#password")
+  // Obtener los input del form
+  const nombreInput = document.querySelector("#nombre");
+  const apellidoInput = document.querySelector("#apellido");
+  const emailInput = document.querySelector("#email");
+  const passwordInput = document.querySelector("#password");
 
-    // Obtener los valores de los campos de entrada
-    const nombreValue = nombreInput.value;
-    const apellidoValue = apellidoInput.value;
-    const emailValue = emailInput.value;
-    const passwordValue = passwordInput.value
+  // Obtener los valores de los campos de entrada
+  const nombreValue = nombreInput.value;
+  const apellidoValue = apellidoInput.value;
+  const emailValue = emailInput.value;
+  const passwordValue = passwordInput.value;
 
-    // Devolver los valores en un objeto
-    return {
-      nombre: nombreValue,
-      apellido: apellidoValue,
-      email: emailValue,
-      password: passwordValue
-    };
-  }
+  // Devolver los valores en un objeto
+  return {
+    nombre: nombreValue,
+    apellido: apellidoValue,
+    email: emailValue,
+    password: passwordValue,
+  };
+}
 
 const userRegister = async (e) => {
   e.preventDefault();
   const objectToSend = getInputValues();
   try {
     await axios.post("../../../createuser", objectToSend);
-    window.location.href = `../login/Login.html`;
+    window.location.href = `../logIn/login.html`;
   } catch (error) {
     console.log(error);
   }
