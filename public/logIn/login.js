8a6c55a1d6942b3bea82512c4901b862803ef4a8
Daniela Quinteros <danielaquinteros.a@gmail.com>
2023-11-02 20:30:30 -0300

    TERMINADO

--- public/logIn/login.js
+++ public/logIn/login.js
@@ -1,6 +1,5 @@
-// console.log("Hola!");
-
 const objectToSend = {};
+
 function getInputValues() {
   const inputs = document.querySelectorAll("input");
   inputs.forEach((input) => (objectToSend[input.id] = input.value));
@@ -9,23 +8,62 @@ function getInputValues() {
 const redirect = () => {
   window.location.href = `../index.html`;
 };
+  const showErrorMessage = (message) => {
+    swal("Ups!", message, "warning");
+  };
 
 const loginUser = async (e) => {
   e.preventDefault();
   getInputValues();
   try {
-    const response = await axios.post(
-      "../../../login",
-      objectToSend
-    );
-    // console.log(response, "respuesta");
-    redirect();
+    const response = await axios.post("../../../login", objectToSend);
+    // Verificar si la solicitud fue exitosa
+    if (response.status === 200) {
+      redirect();
+    }
   } catch (error) {
-    console.log(error.message);
+    // Mostrar mensaje de error al usuario
+    showErrorMessage(error.response.data.message);
   }
 };
+
 const loginButton = document.querySelector(".boton");
 loginButton.addEventListener("click", (e) => loginUser(e));
+
+
+
+// console.log("Hola!");
+
+// const objectToSend = {};
+// function getInputValues() {
+//   const inputs = document.querySelectorAll("input");
+//   inputs.forEach((input) => (objectToSend[input.id] = input.value));
+// }
+
+// const redirect = () => {
+//   window.location.href = `../index.html`;
+// };
+
+// const loginUser = async (e) => {
+//   e.preventDefault();
+//   getInputValues();
+//   try {
+//     const response = await axios.post(
+//       "../../../login",
+//       objectToSend
+//     );
+//     // console.log(response, "respuesta");
+//     redirect();
+//   } catch (error) {
+//     console.log(error.message);
+//   }
+// };
+// const loginButton = document.querySelector(".boton");
+// loginButton.addEventListener("click", (e) => loginUser(e));
+
+
+
+
 
 
 // const objectToSend = {};
